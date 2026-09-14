6d0e5aeda5d1008ddc84f63e2d585472a74eb134
julioau15 <julioaugustov@gmail.com>
2026-09-14 11:21:54 -0300

    first commit

---
+++ app/src/main/java/com/aulasandroid/exercicio01_game_layout/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.aulasandroid.exercicio01_game_layout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.aulasandroid.exercicio01_game_layout.ui.theme.Exercicio01gamelayoutTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Exercicio01gamelayoutTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Exercicio01gamelayoutTheme {
+        Greeting("Android")
+    }
+}
